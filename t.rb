6975ed8f35efbdec794caf4d9344b2b236156138
athoo <athoo@sina.com>
2015-04-07 20:07:23 +0800

    start a new method

---
+++ t.rb
@@ -0,0 +1,16 @@
+class Test
+   @x="bbbb"    
+
+    def self.bb
+
+    puts "this is bb"
+    end
+
+    def self.printing
+        puts @x    
+    bb
+        puts "this is hello printing"
+    end
+end
+
+Test.printing
