92efc8270b09f7a65bddf280b0662668c3d32954
athoo <athoo@sina.com>
2015-06-04 09:22:42 +0800

    check

--- app.rb
+++ app.rb
@@ -20,20 +20,20 @@ class ListingService < Sinatra::Base
       # hash_data.to_json
     end
 
-    # this function send all the gem which has a github url
-    get '/github/collection/yesterday' do
-      hash_data = RubyGem.github_yesterday_json
-      queue = GemMiner::GemMapQueue.new('GemMap')
-      queue.send_message_batch(hash_data)
-      "success"
-    end
+    # # this function send all the gem which has a github url
+    # get '/github/collection/yesterday' do
+    #   hash_data = RubyGem.github_yesterday_json
+    #   queue = GemMiner::GemMapQueue.new('GemMap')
+    #   queue.send_message_batch(hash_data)
+    #   "success"
+    # end
 
     get '/workflow_get_all_gem_info' do
       RubyGem.workflow_get_all_gem_info
       "success"
     end
 
-    get '/workflow_get_sample_gem_info' do
+    get '/github/collection/yesterday' do
       RubyGem.workflow_get_sample_gem_info
       "success"
     end
